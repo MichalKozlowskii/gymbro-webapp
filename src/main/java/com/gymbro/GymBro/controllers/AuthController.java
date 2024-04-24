41369d238f8483103681634bec9e9b8ade504622
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-04-24 21:10:01 +0200

    feat: Auth controller

---
+++ src/main/java/com/gymbro/GymBro/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.gymbro.GymBro.controllers;
+
+import com.gymbro.GymBro.models.UserEntity;
+import com.gymbro.GymBro.services.UserService;
+import com.gymbro.GymBro.web.DTO.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Validated @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+
+        UserEntity existingUser = userService.findUserByName(userDto.getName());
+
+        if (existingUser != null && existingUser.getName() != null && !existingUser.getName().isEmpty()) {
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same name.");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
