460cd81c39680651835df315fb6485d9e8185b94
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-04-30 17:50:31 +0200

    feat: Exercise DTO

---
+++ src/main/java/com/gymbro/GymBro/web/DTO/ExerciseDto.java
@@ -0,0 +1,74 @@
+package com.gymbro.GymBro.web.DTO;
+
+import java.util.Objects;
+
+public class ExerciseDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Long userId;
+
+    public ExerciseDto(Long id, String name, String description, Long userId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.userId = userId;
+    }
+
+    public ExerciseDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ExerciseDto that = (ExerciseDto) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        if (!Objects.equals(description, that.description)) return false;
+        return Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        return result;
+    }
+}
