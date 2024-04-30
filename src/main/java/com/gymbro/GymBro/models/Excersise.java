fccec8f7a18b6d50634f97119e8aaaff76bd4971
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-04-30 16:55:32 +0200

    feat: exercise entity

---
+++ src/main/java/com/gymbro/GymBro/models/Excersise.java
@@ -0,0 +1,64 @@
+package com.gymbro.GymBro.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "excersises")
+public class Excersise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    String name;
+    String description;
+
+    public Excersise(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Excersise excersise = (Excersise) o;
+
+        if (!Objects.equals(id, excersise.id)) return false;
+        if (!Objects.equals(name, excersise.name)) return false;
+        return Objects.equals(description, excersise.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
