9e9a356697e4beb21a0566e85fd99eae0ee30af2
Michał <michal@saturdaysheroes.dev>
2024-05-03 16:41:53 +0200

    feat: updateExerciseById repository method

--- src/main/java/com/gymbro/GymBro/repositories/ExerciseRepository.java
+++ src/main/java/com/gymbro/GymBro/repositories/ExerciseRepository.java
@@ -3,6 +3,8 @@ package com.gymbro.GymBro.repositories;
 import com.gymbro.GymBro.models.Exercise;
 import com.gymbro.GymBro.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     List<Exercise> findByUser(UserEntity user);
+    @Modifying
+    @Query("update Exercise e set e.name = ?1, e.description = ?2 where e.id = ?3")
+    void setUserInfoById(String name, String description, Long id);
 }
