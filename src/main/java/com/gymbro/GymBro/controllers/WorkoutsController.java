b7adf3090f7d0d2c69ad7e923fe1aaba3c56221e
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-05-15 18:14:10 +0200

    feat: handle get request at /workouts

--- src/main/java/com/gymbro/GymBro/controllers/WorkoutsController.java
+++ src/main/java/com/gymbro/GymBro/controllers/WorkoutsController.java
@@ -1,5 +1,7 @@
 package com.gymbro.GymBro.controllers;
 
+import com.gymbro.GymBro.services.WorkoutService;
+import com.gymbro.GymBro.web.DTO.WorkoutDto;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -7,11 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class WorkoutsController {
 
+    private final WorkoutService workoutService;
+
+    public WorkoutsController(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
+
     @GetMapping("/workouts")
     public String getWorkouts(Model model, @AuthenticationPrincipal User user) {
+        List<WorkoutDto> workouts = workoutService.findAllWorkoutsOfUser(user);
+
+        model.addAttribute("workouts", workouts);
+
         return "workouts";
     }
 }
