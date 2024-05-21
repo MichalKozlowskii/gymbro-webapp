86530b9ffe684e2e41955325a3519f47fdbe7fe2
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-05-21 22:34:45 +0200

    feat: display exercises' stats sorted by weight descending

--- src/main/java/com/gymbro/GymBro/controllers/StatsController.java
+++ src/main/java/com/gymbro/GymBro/controllers/StatsController.java
@@ -10,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class StatsController {
@@ -25,8 +27,15 @@ public class StatsController {
     @GetMapping("/stats")
     public String getStats(Model model, @AuthenticationPrincipal User user) {
         Map<ExerciseDto, List<SetDto>> exerciseBestSetsMap = statsService.findBestSetsOfUsersExercises(user);
-
         model.addAttribute("exerciseBestSetsMap", exerciseBestSetsMap);
+
+        List<ExerciseDto> exercisesSortedByWeight = exerciseBestSetsMap.keySet().stream()
+                        .sorted(Comparator.comparing(exerciseDto ->
+                                exerciseBestSetsMap.get(exerciseDto).get(1).getWeight(),
+                                Comparator.reverseOrder()
+                        ))
+                        .toList();
+        model.addAttribute("exercises", exercisesSortedByWeight);
 
         return "stats";
     }
