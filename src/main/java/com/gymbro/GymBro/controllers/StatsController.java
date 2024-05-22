61d4e2739ffa170ea3a5c10addc1eb012d79ea9d
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-05-22 17:25:16 +0200

    feat: sort functionality

--- src/main/java/com/gymbro/GymBro/controllers/StatsController.java
+++ src/main/java/com/gymbro/GymBro/controllers/StatsController.java
@@ -7,13 +7,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @Controller
 public class StatsController {
@@ -31,11 +27,69 @@ public class StatsController {
 
         List<ExerciseDto> exercisesSortedByWeight = exerciseBestSetsMap.keySet().stream()
                         .sorted(Comparator.comparing(exerciseDto ->
-                                exerciseBestSetsMap.get(exerciseDto).get(1).getWeight(),
+                                exerciseBestSetsMap.get(exerciseDto).get(0).getWeight(),
                                 Comparator.reverseOrder()
                         ))
                         .toList();
         model.addAttribute("exercises", exercisesSortedByWeight);
+
+        return "stats";
+    }
+
+    @GetMapping("/stats/sorted")
+    public String sortExercises(@RequestParam("sortOptions") int value,
+                                Model model,
+                                @AuthenticationPrincipal User user) {
+        // 1 - Weight: Descending
+        // 2 - Weight: Ascending
+        // 3 - Reps: Descending
+        // 4 - Reps: Ascending
+
+        Map<ExerciseDto, List<SetDto>> exerciseBestSetsMap = statsService.findBestSetsOfUsersExercises(user);
+        model.addAttribute("exerciseBestSetsMap", exerciseBestSetsMap);
+
+        List<ExerciseDto> exercisesSorted = new ArrayList<>();
+
+        switch (value) {
+            case 1: {
+                exercisesSorted = exerciseBestSetsMap.keySet().stream()
+                        .sorted(Comparator.comparing(exerciseDto ->
+                                        exerciseBestSetsMap.get(exerciseDto).get(0).getWeight(),
+                                Comparator.reverseOrder()
+                        ))
+                        .toList();
+                break;
+            }
+
+            case 2: {
+                exercisesSorted = exerciseBestSetsMap.keySet().stream()
+                        .sorted(Comparator.comparing(exerciseDto ->
+                                        exerciseBestSetsMap.get(exerciseDto).get(0).getWeight()
+                        ))
+                        .toList();
+                break;
+            }
+
+            case 3: {
+                exercisesSorted = exerciseBestSetsMap.keySet().stream()
+                        .sorted(Comparator.comparing(exerciseDto ->
+                                        exerciseBestSetsMap.get(exerciseDto).get(1).getReps(),
+                                Comparator.reverseOrder()
+                        ))
+                        .toList();
+                break;
+            }
+
+            case 4: {
+                exercisesSorted = exerciseBestSetsMap.keySet().stream()
+                        .sorted(Comparator.comparing(exerciseDto ->
+                                        exerciseBestSetsMap.get(exerciseDto).get(1).getReps()
+                        ))
+                        .toList();
+                break;
+            }
+        }
+        model.addAttribute("exercises", exercisesSorted);
 
         return "stats";
     }
