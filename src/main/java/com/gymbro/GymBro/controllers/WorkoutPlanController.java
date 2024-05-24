4ffc6097a95c651c6beba89d02a384e9ddf28676
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-05-24 18:40:58 +0200

    refactor: adding workout plans

--- src/main/java/com/gymbro/GymBro/controllers/WorkoutPlanController.java
+++ src/main/java/com/gymbro/GymBro/controllers/WorkoutPlanController.java
@@ -7,6 +7,7 @@ import com.gymbro.GymBro.services.ExerciseService;
 import com.gymbro.GymBro.services.UserService;
 import com.gymbro.GymBro.services.WorkoutPlanService;
 import com.gymbro.GymBro.services.WorkoutService;
+import com.gymbro.GymBro.web.DTO.ExerciseDto;
 import com.gymbro.GymBro.web.DTO.WorkoutPlanDto;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -15,7 +16,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
@@ -33,26 +36,23 @@ public class WorkoutPlanController {
         this.workoutService = workoutService;
     }
 
-    private boolean validateWorkoutPlan(WorkoutPlanDto workoutPlanDto, BindingResult result) {
+    private boolean validateWorkoutPlan(WorkoutPlanDto workoutPlanDto, List<Long> exerciseIds, BindingResult result) {
         if (workoutPlanDto.getName() == null || Objects.equals(workoutPlanDto.getName(), "")) {
             result.rejectValue("name", null, "Name field can't be blank!");
         }
 
-        List<Long> exerciseIds = workoutPlanDto.getExercisesIds();
         exerciseIds.removeIf(Objects::isNull);
+        if (exerciseIds.isEmpty()) {
+            result.rejectValue("name", null, "You have to select exercises!");
+        }
 
         List<Integer> sets = workoutPlanDto.getSets();
         sets.removeIf(Objects::isNull);
-
         List<Integer> reps = workoutPlanDto.getReps();
         reps.removeIf(Objects::isNull);
 
         boolean hasErrors = false;
 
-        if (workoutPlanDto.getExercisesIds() == null || exerciseIds.isEmpty()) {
-            result.rejectValue("name", null, "You have to select exercises!");
-            hasErrors = true;
-        }
         if (workoutPlanDto.getSets() == null || sets.isEmpty()) {
             result.rejectValue("name", null, "You have to specify amount of sets for each exercise!");
             hasErrors = true;
@@ -70,7 +70,6 @@ public class WorkoutPlanController {
             return false;
         }
 
-        workoutPlanDto.setExercisesIds(exerciseIds);
         workoutPlanDto.setSets(sets);
         workoutPlanDto.setReps(reps);
 
@@ -88,7 +87,6 @@ public class WorkoutPlanController {
     public String showAddWorkoutPlanForm(Model model, @AuthenticationPrincipal User user) {
         WorkoutPlanDto workoutPlanDto = new WorkoutPlanDto();
 
-        model.addAttribute("workoutPlan", workoutPlanDto);
         model.addAttribute("exercisesDto",
                 exerciseService.findAllExercisesOfUser(user));
 
@@ -97,25 +95,61 @@ public class WorkoutPlanController {
 
     @PostMapping("addworkoutplan/save")
     public String saveWorkoutPlan(@ModelAttribute("workoutPlan") WorkoutPlanDto workoutPlanDto,
+                                  @RequestParam Map<String, String> params,
                                   BindingResult result,
                                   Model model,
                                   @AuthenticationPrincipal User user) {
 
-        if (!validateWorkoutPlan(workoutPlanDto, result)) {
-            model.addAttribute("workoutPlan", workoutPlanDto);
-            model.addAttribute("exercisesDto", exerciseService.findAllExercisesOfUser(user));
+        String name = params.get("name");
+        if (name == null || name.isEmpty()) {
+            model.addAttribute("errorName", "Name is required!");
+            model.addAttribute("exercisesDto",
+                    exerciseService.findAllExercisesOfUser(user));
             return "addworkoutplan";
         }
 
+        List<Long> exerciseIds = new ArrayList<>();
+        List<Integer> sets = new ArrayList<>();
+        List<Integer> reps = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String element = entry.getKey().substring(0, entry.getKey().length() - 1);
+            String val = entry.getValue();
+
+            if (!val.isEmpty() && val.length() < "exercisesIds".length()) {
+                switch (element) {
+                    case "exercisesIds" -> exerciseIds.add(Long.parseLong(val));
+                    case "sets" -> sets.add(Integer.parseInt(val));
+                    case "reps" -> reps.add(Integer.parseInt(val));
+                }
+            }
+        }
+
+        if (exerciseIds.isEmpty() || sets.isEmpty() || reps.isEmpty() || reps.size() != sets.size() || sets.size() != exerciseIds.size()) {
+            model.addAttribute("errorName", "Exercises form wasn't completed correctly!");
+            model.addAttribute("exercisesDto",
+                    exerciseService.findAllExercisesOfUser(user));
+            return "addworkoutplan";
+        }
+
+        workoutPlanDto.setSets(sets);
+        workoutPlanDto.setReps(reps);
+
         Long userId = userService.findUserByName(user.getUsername()).getId();
         workoutPlanDto.setUserId(userId);
+
+        List<ExerciseDto> exerciseDtoList = exerciseIds.stream()
+                                .map(exerciseService::findExerciseById)
+                                .map(exerciseService::mapToExerciseDto)
+                                .toList();
+        workoutPlanDto.setExercises(exerciseDtoList);
 
         workoutPlanService.saveWorkoutPlan(workoutPlanDto);
 
         return "redirect:/workoutplans?addsuccess";
     }
 
-    @GetMapping("workoutplans/edit/{id}")
+    /*@GetMapping("workoutplans/edit/{id}")
     public String showEditWorkoutPlanForm(@PathVariable Long id, Model model, @AuthenticationPrincipal User user) {
         WorkoutPlan workoutPlan = workoutPlanService.findWorkoutPlanById(id);
         WorkoutPlanDto workoutPlanDto = workoutPlanService.mapToWorkoutPlanDto(workoutPlan);
@@ -190,5 +224,5 @@ public class WorkoutPlanController {
         workoutPlanService.deleteWorkoutPlanById(id);
 
         return "redirect:/workoutplans?deletesuccess";
-    }
+    }*/
 }
