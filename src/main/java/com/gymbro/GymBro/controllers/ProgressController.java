b5607e3e532482b5c0eb127106f127aa78dc3ad2
Michał Kozłowski <michal.kozlowski032@gmail.com>
2024-05-26 17:59:00 +0200

    feat: display sets of two last workouts

--- src/main/java/com/gymbro/GymBro/controllers/ProgressController.java
+++ src/main/java/com/gymbro/GymBro/controllers/ProgressController.java
@@ -1,18 +1,49 @@
 package com.gymbro.GymBro.controllers;
 
+import com.gymbro.GymBro.models.Workout;
+import com.gymbro.GymBro.models.WorkoutPlan;
+import com.gymbro.GymBro.records.Pair;
+import com.gymbro.GymBro.services.ProgressService;
+import com.gymbro.GymBro.services.WorkoutPlanService;
 import com.gymbro.GymBro.services.WorkoutService;
+import com.gymbro.GymBro.web.DTO.WorkoutDto;
+import com.gymbro.GymBro.web.DTO.WorkoutPlanDto;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ProgressController {
+    private final WorkoutService workoutService;
+    private final WorkoutPlanService workoutPlanService;
+    private final ProgressService progressService;
+
+    public ProgressController(WorkoutService workoutService,WorkoutPlanService workoutPlanService, ProgressService progressService) {
+        this.workoutService = workoutService;
+        this.progressService = progressService;
+        this.workoutPlanService = workoutPlanService;
+    }
 
     @GetMapping("/progress")
     public String getProgress(Model model, @AuthenticationPrincipal User user) {
+        List<WorkoutPlanDto> workoutPlans = workoutPlanService.findAllWorkoutPlansDtoOfUser(user);
+        Map<WorkoutPlanDto, Pair<WorkoutDto, WorkoutDto>> workoutPlan2LastWorkouts = new HashMap<>();
+
+        for (WorkoutPlanDto workoutPlan : workoutPlans) {
+            Pair<WorkoutDto, WorkoutDto> lastWorkoutsPair = progressService.findTwoLastWorkoutsOfWorkoutPlanById(workoutPlan.getId());
+
+            if (lastWorkoutsPair.key() != null || lastWorkoutsPair.value() != null) {
+                workoutPlan2LastWorkouts.put(workoutPlan, lastWorkoutsPair);
+            }
+        }
+
+        model.addAttribute("workoutPlan2LastWorkoutsMap", workoutPlan2LastWorkouts);
 
         return "progress";
     }
